07bdc7e13e3d55e0db1f935e61a7a81a79d0f2a0
ucreates <ucreates.inc@gmail.com>
2020-03-14 02:16:44 +0900

    add circle1 component

---
+++ android-java-gles1renderer/src/main/java/com/ucreates/renderer/asset/mesh/CircleAsset1.java
@@ -0,0 +1,75 @@
+// ======================================================================
+// Project Name    : android_renderer
+//
+// Copyright © 2020 U-CREATES. All rights reserved.
+//
+// This source code is the property of U-CREATES.
+// If such findings are accepted at any time.
+// We hope the tips and helpful in developing.
+// ======================================================================
+package com.ucreates.renderer.asset.mesh;
+import android.opengl.GLES11;
+import com.ucreates.renderer.asset.BaseAsset;
+import com.ucreates.renderer.entity.GLESColor;
+import com.ucreates.renderer.entity.VertexArray;
+import com.ucreates.renderer.io.memory.Allocator;
+import com.ucreates.renderer.math.GLES1Angle;
+import com.ucreates.renderer.renderer.GLES1Renderer;
+public class CircleAsset1 extends BaseAsset {
+    private int divideCount;
+    private float radius;
+    public CircleAsset1(float radius, int divideCount, GLESColor color) {
+        this.radius = radius;
+        this.color = color;
+        this.divideCount = divideCount;
+        this.renderMode = GLES11.GL_TRIANGLES;
+        this.vertex = new VertexArray(GLES1Renderer.DIMENSION2D);
+    }
+    @Override
+    public void create() {
+        int vertexCount = this.divideCount * 3;
+        int verticesLength = vertexCount * GLES1Renderer.DIMENSION2D;
+        int vertexMemsize = verticesLength * Allocator.GL_FLOAT_SIZE;
+        int colorsLength = vertexCount * GLES1Renderer.RGBA;
+        int colorsMemsize = colorsLength * Allocator.GL_FLOAT_SIZE;
+        float[] vertices = new float[vertexMemsize];
+        float[] colors = new float[colorsMemsize];
+        int didx = 0;
+        for (int i = 0; i < verticesLength; i += 6) {
+            double unitDegree = (360.0d / this.divideCount);
+            double degree1 = didx * unitDegree;
+            double degree2 = (didx + 1) * unitDegree;
+            double radian1 = GLES1Angle.toRadian(degree1);
+            double radian2 = GLES1Angle.toRadian(degree2);
+            float x1 = (float) Math.cos(radian1) * this.radius;
+            float y1 = (float) Math.sin(radian1) * this.radius;
+            float x2 = (float) Math.cos(radian2) * this.radius;
+            float y2 = (float) Math.sin(radian2) * this.radius;
+            vertices[i] = 0;
+            vertices[i + 1] = 0;
+            vertices[i + 2] = x1;
+            vertices[i + 3] = y1;
+            vertices[i + 4] = x2;
+            vertices[i + 5] = y2;
+            didx++;
+        }
+        for (int i = 0; i < colorsLength; i += 12) {
+            colors[i] = this.color.r;
+            colors[i + 1] = this.color.g;
+            colors[i + 2] = this.color.b;
+            colors[i + 3] = this.color.a;
+            colors[i + 4] = this.color.r;
+            colors[i + 5] = this.color.g;
+            colors[i + 6] = this.color.b;
+            colors[i + 7] = this.color.a;
+            colors[i + 8] = this.color.r;
+            colors[i + 9] = this.color.g;
+            colors[i + 10] = this.color.b;
+            colors[i + 11] = this.color.a;
+        }
+        this.vertex.setVertexCount(vertexCount);
+        this.vertex.setVertices(vertices);
+        this.vertex.setColors(colors);
+        return;
+    }
+}
---
+++ android-java-gles1renderer/src/main/java/com/ucreates/renderer/math/GLES1Angle.java
@@ -0,0 +1,16 @@
+// ======================================================================
+// Project Name    : android_renderer
+//
+// Copyright © 2020 U-CREATES. All rights reserved.
+//
+// This source code is the property of U-CREATES.
+// If such findings are accepted at any time.
+// We hope the tips and helpful in developing.
+// ======================================================================
+package com.ucreates.renderer.math;
+import java.lang.Math;
+public class GLES1Angle {
+    public static double toRadian(double degree) {
+        return degree * Math.PI / 180.0;
+    }
+}
