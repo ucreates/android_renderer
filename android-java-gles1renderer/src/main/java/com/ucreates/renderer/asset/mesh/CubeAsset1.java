15eabbce4b3327cbf5dead9b33dded3e19d8f75b
ucreates <ucreates.inc@gmail.com>
2020-03-21 00:46:08 +0900

    add sphere1 component

--- android-java-gles1renderer/src/main/java/com/ucreates/renderer/asset/mesh/CubeAsset1.java
+++ android-java-gles1renderer/src/main/java/com/ucreates/renderer/asset/mesh/CubeAsset1.java
@@ -9,9 +9,12 @@
 // ======================================================================
 package com.ucreates.renderer.asset.mesh;
 import android.opengl.GLES11;
+import android.renderscript.Float3;
 import com.ucreates.renderer.asset.BaseAsset;
 import com.ucreates.renderer.entity.GLESColor;
 import com.ucreates.renderer.entity.VertexArray;
+import com.ucreates.renderer.io.memory.Allocator;
+import com.ucreates.renderer.math.GLES1Normal;
 public class CubeAsset1 extends BaseAsset {
     public CubeAsset1(float width, float height, float depth, GLESColor color) {
         this.width = width;
@@ -298,10 +301,34 @@ public class CubeAsset1 extends BaseAsset {
             this.color.a,
         };
         int vertexCount = verticies.length / this.vertex.dimension;
+        int normalsLength = verticies.length;
+        int normalsMemsize = normalsLength * Allocator.GL_FLOAT_SIZE;
+        float[] normals = new float[normalsMemsize];
+        for (int i = 0; i < verticies.length; i += 9) {
+            float x1 = verticies[i];
+            float y1 = verticies[i + 1];
+            float z1 = verticies[i + 2];
+            float x2 = verticies[i + 3];
+            float y2 = verticies[i + 4];
+            float z2 = verticies[i + 5];
+            float x3 = verticies[i + 6];
+            float y3 = verticies[i + 7];
+            float z3 = verticies[i + 8];
+            Float3 triangleNormal = GLES1Normal.toNormal(x1, y1, z1, x2, y2, z2, x3, y3, z3);
+            normals[i] = triangleNormal.x;
+            normals[i + 1] = triangleNormal.y;
+            normals[i + 2] = triangleNormal.z;
+            normals[i + 3] = triangleNormal.x;
+            normals[i + 4] = triangleNormal.y;
+            normals[i + 5] = triangleNormal.z;
+            normals[i + 6] = triangleNormal.x;
+            normals[i + 7] = triangleNormal.y;
+            normals[i + 8] = triangleNormal.z;
+        }
         this.vertex.setVertexCount(vertexCount);
         this.vertex.setVertices(verticies);
         this.vertex.setColors(vertexColors);
-        this.vertex.setNormals(verticies);
+        this.vertex.setNormals(normals);
         return;
     }
 }
