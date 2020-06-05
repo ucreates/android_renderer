f4b107603e09e26c947cd94597bc536054f7d2e0
ucreates <ucreates.inc@gmail.com>
2020-06-06 00:00:58 +0900

    add texture atlas animator component

---
+++ android-java-gles1renderer/src/main/java/com/ucreates/renderer/asset/TextureAtlasAnimatorAsset.java
@@ -0,0 +1,47 @@
+// ======================================================================
+// Project Name    : android_renderer
+//
+// Copyright © 2020 U-CREATES. All rights reserved.
+//
+// This source code is the property of U-CREATES.
+// If such findings are accepted at any time.
+// We hope the tips and helpful in developing.
+// ======================================================================
+package com.ucreates.renderer.asset;
+import com.ucreates.renderer.io.memory.Allocator;
+import java.nio.FloatBuffer;
+import java.util.ArrayList;
+public class TextureAtlasAnimatorAsset extends BaseAnimatorAsset {
+    private BaseAsset textureAtlasAsset;
+    private ArrayList<FloatBuffer> animationFrameUVs = new ArrayList<FloatBuffer>();
+    public void setTextureAtlas(BaseAsset asset) {
+        this.textureAtlasAsset = asset;
+        return;
+    }
+    public void addFrameUVs(float[] uvs) {
+        FloatBuffer tmpUVs = Allocator.allocateFloat(uvs.length);
+        tmpUVs.put(uvs).position(0);
+        this.animationFrameUVs.add(tmpUVs);
+        return;
+    }
+    @Override
+    public BaseAsset getCurrentFrame() {
+        FloatBuffer uvs = null;
+        int index = this.currentTextureIndex % (int) this.animationFrameUVs.size();
+        if (this.frameSpan != this.currentFrame) {
+            uvs = this.animationFrameUVs.get(index);
+            this.currentFrame++;
+        } else {
+            uvs = this.animationFrameUVs.get(index);
+            if (this.currentTextureIndex == this.animationFrameUVs.size() - 1) {
+                this.currentTextureIndex = 0;
+            } else {
+                this.currentTextureIndex++;
+            }
+            this.currentFrame = 0;
+        }
+        this.textureAtlasAsset.transform = this.transform;
+        this.textureAtlasAsset.vertex.uvs = uvs;
+        return this.textureAtlasAsset;
+    }
+}
