cd4fb25c31a6ed8637a75b78b9b4eb0c677aa46c
ucreates <ucreates.inc@gmail.com>
2020-10-10 00:01:56 +0900

    add rectangle3 texture component

--- android-java-gles1renderer/src/main/java/com/ucreates/renderer/asset/polygon/RectangleAsset3.java
+++ android-java-gles1renderer/src/main/java/com/ucreates/renderer/asset/polygon/RectangleAsset3.java
@@ -8,8 +8,10 @@
 // We hope the tips and helpful in developing.
 // ======================================================================
 package com.ucreates.renderer.asset.polygon;
+import android.content.Context;
 import android.opengl.GLES11;
 import com.ucreates.renderer.asset.BaseAsset;
+import com.ucreates.renderer.asset.TextureAsset;
 import com.ucreates.renderer.entity.GLESColor;
 import com.ucreates.renderer.entity.VertexArray;
 import com.ucreates.renderer.renderer.GLES1Renderer;
@@ -65,6 +67,69 @@ public class RectangleAsset3 extends BaseAsset {
         this.vertex.setVertexCount(vertexCount);
         this.vertex.setVertices(vertices);
         this.vertex.setColors(colors);
+        return;
+    }
+    @Override
+    public void create(String texturePath, Context context) {
+        this.texture = new TextureAsset();
+        this.texture.load(texturePath, context);
+        float x = 0.5f * this.width;
+        float y = 0.5f * this.height;
+        float vertices[] = {
+            // left down
+            -x,
+            -y,
+            // right down
+            x,
+            -y,
+            // left top
+            -x,
+            y,
+            // right top
+            x,
+            y,
+        };
+        float colors[] = {
+            // left down
+            this.color.r,
+            this.color.g,
+            this.color.b,
+            this.color.a,
+            // right down
+            this.color.r,
+            this.color.g,
+            this.color.b,
+            this.color.a,
+            // left top
+            this.color.r,
+            this.color.g,
+            this.color.b,
+            this.color.a,
+            // right top
+            this.color.r,
+            this.color.g,
+            this.color.b,
+            this.color.a,
+        };
+        float uvs[] = {
+            // left down
+            0.0f,
+            1.0f,
+            // right down
+            1.0f,
+            1.0f,
+            // left up
+            0.0f,
+            0.0f,
+            // right up
+            1.0f,
+            0.0f,
+        };
+        int vertexCount = vertices.length / this.vertex.dimension;
+        this.vertex.setVertexCount(vertexCount);
+        this.vertex.setVertices(vertices);
+        this.vertex.setColors(colors);
+        this.vertex.setUVs(uvs);
         return;
     }
 }
