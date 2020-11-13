ac6ac4173cc0c1f869df6d8e0f9cd0b63b895296
ucreates <ucreates.inc@gmail.com>
2020-11-14 00:09:01 +0900

    add rectangle4 component

---
+++ android-java-gles1renderer/src/main/java/com/ucreates/renderer/asset/polygon/RectangleAsset4.java
@@ -0,0 +1,74 @@
+// ======================================================================
+// Project Name    : android_renderer
+//
+// Copyright © 2020 U-CREATES. All rights reserved.
+//
+// This source code is the property of U-CREATES.
+// If such findings are accepted at any time.
+// We hope the tips and helpful in developing.
+// ======================================================================
+package com.ucreates.renderer.asset.polygon;
+import android.content.Context;
+import android.opengl.GLES11;
+import com.ucreates.renderer.asset.BaseAsset;
+import com.ucreates.renderer.asset.TextureAsset;
+import com.ucreates.renderer.entity.GLESColor;
+import com.ucreates.renderer.entity.VertexArray;
+import com.ucreates.renderer.renderer.GLES1Renderer;
+public class RectangleAsset4 extends BaseAsset {
+    public RectangleAsset4(float width, float height, GLESColor color) {
+        this.width = width;
+        this.height = height;
+        this.color = color;
+        this.renderMode = GLES11.GL_TRIANGLE_STRIP;
+        this.vertex = new VertexArray(GLES1Renderer.DIMENSION2D);
+    }
+    @Override
+    public void create() {
+        float x = 0.5f * this.width;
+        float y = 0.5f * this.height;
+        float vertices[] = {
+            // left down
+            -x,
+            -y,
+            // right down
+            x,
+            -y,
+            // left top
+            -x,
+            y,
+            // right top
+            x,
+            y,
+        };
+        float colors[] = {
+            // left down
+            this.color.r,
+            this.color.g,
+            this.color.b,
+            this.color.a,
+            // right down
+            this.color.r,
+            this.color.g,
+            this.color.b,
+            this.color.a,
+            // left top
+            this.color.r,
+            this.color.g,
+            this.color.b,
+            this.color.a,
+            // right top
+            this.color.r,
+            this.color.g,
+            this.color.b,
+            this.color.a,
+        };
+        short indicies[] = {0, 1, 2, 3};
+        int vertexCount = vertices.length / this.vertex.dimension;
+        this.vertex.setVertexCount(vertexCount);
+        this.vertex.setVertices(vertices);
+        this.vertex.setColors(colors);
+        this.vertex.setIndicies(indicies);
+        return;
+    }
+}
